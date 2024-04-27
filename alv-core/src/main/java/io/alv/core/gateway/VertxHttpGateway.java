2511d8986f71694b5b7f7953c4790a83586672db
Reef3rm4n <j.benato@proton.me>
2024-04-27 13:13:02 +0200

    - updated README.md

--- alv-core/src/main/java/io/alv/core/gateway/VertxHttpGateway.java
+++ alv-core/src/main/java/io/alv/core/gateway/VertxHttpGateway.java
@@ -1,6 +1,21 @@
 package io.alv.core.gateway;
 
 import io.smallrye.mutiny.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
 
 public class VertxHttpGateway extends AbstractVerticle {
+
+
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    // todo load http/websocket/grpc routes from MessageHandlers
+    // todo handle authorization
+    // todo handle validation
+    // todo handle broadcast subscriptions
+  }
+
+  @Override
+  public void stop(Promise<Void> stopPromise) throws Exception {
+    super.stop(stopPromise);
+  }
 }
